9016f130499e71133aa780704d86d4a2e1140925
Pauline GANGLOFF <pganglof@e2r4p12.42.fr>
2020-01-30 19:17:12 +0100

    debut de tentative de pipe, dup

--- srcs/exec_command/exec_command.c
+++ srcs/exec_command/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: pganglof <pganglof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 17:33:16 by pganglof          #+#    #+#             */
-/*   Updated: 2020/01/30 18:31:47 by pganglof         ###   ########.fr       */
+/*   Updated: 2020/01/30 19:16:16 by pganglof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,38 @@
 int			exec_command(t_data *data)
 {
 	pid_t	pid;
-	
+	// int		savestdout;
+
+	pipe(data->mypipefd);
 	if (!data->lst_parsing->next)
 	{
 		pid = fork();
 		if (pid == 0)
 		{
 			// printf("TEST1\n");
+			// if (((t_parsing*)(data->lst_parsing->content))->l_chevron)
+				// dup2(data->mypipefd[1], STDOUT_FILENO);
 			if (!is_builtin((t_parsing*)(data->lst_parsing->content)))
 				if (!(ft_execve(((t_parsing*)(data->lst_parsing->content))->arg[0],
 				((t_parsing*)(data->lst_parsing->content))->arg, data->env)))
 					ft_printf("pop & max: command not found: %s\n",
 					((t_parsing*)(data->lst_parsing->content))->arg[0]);
+			// printf("CHILD\n");
+	
+
 		}
 		else if (pid < 0)
 			exit_failure("fork", data);
 		else
+		{
 			waitpid(pid, &data->status, 0);
+			// close(data->mypipefd[1]);
+			// printf("PARENT\n");
+			// dup2(data->mypipefd[0], STDIN_FILENO);
+		}
+		// close(data->mypipefd[0]);
+		// close(data->mypipefd[1]);
+
 		// printf("TEST2\n");
 	}
 	else
