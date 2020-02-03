4b2d8a98d0429e2f21bd2b749d4d8aac1d12dd55
Pauline GANGLOFF <pganglof@e2r7p17.42.fr>
2020-02-03 10:57:48 +0100

    1 pipe fonctionne

--- srcs/exec_command/exec_command.c
+++ srcs/exec_command/exec_command.c
@@ -6,37 +6,55 @@
 /*   By: pganglof <pganglof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 09:42:32 by pganglof          #+#    #+#             */
-/*   Updated: 2020/02/03 10:03:10 by pganglof         ###   ########.fr       */
+/*   Updated: 2020/02/03 10:56:39 by pganglof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void		chevron(t_data *data)
+static void		separator(t_data *data)
 {
-	data->lst_parsing = data->lst_parsing->next;
-	data->mypipefd[1] = open(((t_parsing*)(data->lst_parsing->content))->arg[0],
-	O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-	dup2(data->mypipefd[1], 1);
+	if (((t_parsing*)(data->lst_parsing->content))->l_chevron)
+	{
+		data->lst_parsing = data->lst_parsing->next;
+		data->mypipefd[1] = open(((t_parsing*)(data->lst_parsing->content))->arg[0],
+		O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+	}
+	dup2(data->mypipefd[1], STDOUT_FILENO);
 }
 
 static void		fork_function(t_parsing *tmp, t_data *data)
 {
-	pid_t	pid;
+	pid_t	pid1;
+	pid_t	pid2;
 
-	pid = fork();
-	if (pid == 0)
+	pid1 = fork();
+	if (pid1 == 0)
 	{
-		if (tmp->l_chevron)
-			chevron(data);
+		if (tmp->l_chevron || tmp->pipe)
+			separator(data);
 		if (is_builtin(tmp) == 0)
 			if (ft_execve(tmp->arg[0], tmp->arg, data->env) == 0)
 				ft_printf("pop & max: command not found: %s\n", tmp->arg[0]);
 	}
-	else if (pid < 0)
+	else if (pid1 < 0)
 		exit_failure("fork", data);
 	else
-		waitpid(pid, &data->status, 0);
+	{
+		waitpid(pid1, &data->status, 0);
+		if (tmp->pipe)
+		{
+			data->lst_parsing = data->lst_parsing->next;
+			tmp = data->lst_parsing->content;
+			pid2 = fork();
+			close(data->mypipefd[1]);
+			dup2(data->mypipefd[0], STDIN_FILENO);
+			close(data->mypipefd[0]);
+			if (is_builtin(tmp) == 0)
+				if (ft_execve(tmp->arg[0], tmp->arg, data->env) == 0)
+					ft_printf("pop & max: command not found: %s\n", tmp->arg[0]);
+		}
+	}
 	close(data->mypipefd[0]);		
 	close(data->mypipefd[1]);
 }
