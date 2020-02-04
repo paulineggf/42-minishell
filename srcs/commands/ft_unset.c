8fe1af0d8d85865f2055deb909b3ad130f1ff3f6
Maxime CRAIPEAU <mcraipea@e2r3p12.42.fr>
2020-02-04 14:42:05 +0100

    la fonction unset qui fonctionne

--- srcs/commands/ft_unset.c
+++ srcs/commands/ft_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pganglof <pganglof@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mcraipea <mcraipea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 14:15:14 by mcraipea          #+#    #+#             */
-/*   Updated: 2020/02/04 14:27:57 by pganglof         ###   ########.fr       */
+/*   Updated: 2020/02/04 14:38:29 by mcraipea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ static char			**del_env(int *i, char **env, char *value, t_data *data)
 	int			j;
 	int			k;
 	int			l;
+	int			size;
 	char		**new;
 
 	j = 0;
+	size = ft_strlen(value);
 	while (env[j])
 	{
-		if (ft_strcmp(env[j], value) == 0)
+		if (ft_strncmp(env[j], value, size) == 0)
 		{
 			if (!(new = ft_calloc(*i, sizeof(char*))))
 				exit_failure("malloc", data);
@@ -30,7 +32,7 @@ static char			**del_env(int *i, char **env, char *value, t_data *data)
 			k = 0;
 			while (env[l])
 			{
-				if (ft_strcmp(env[l], value) != 0)
+				if (ft_strncmp(env[l], value, size) != 0)
 				{
 					if (!(new[k] = ft_strdup(env[l])))
 						exit_failure("ft_strdup", data);
@@ -54,18 +56,9 @@ static char			**del_env(int *i, char **env, char *value, t_data *data)
 void			ft_unset(t_parsing *tmp, t_data *data)
 {
 	int			i;
-	char		buffer[128];
 
-	i = 0;
-	while (tmp->arg[1][i] != '=')
-	{
-		buffer[i] = tmp->arg[1][i];
-		i++;
-	}
-	buffer[i] = tmp->arg[1][i];
-	buffer[i] = '\0';
 	i = 0;
 	while (data->env[i])
 		i++;
-	data->env = del_env(&i, data->env, buffer, data);
+	data->env = del_env(&i, data->env, tmp->arg[1], data);
 }
