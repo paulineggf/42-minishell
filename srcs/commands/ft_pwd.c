5cf84ea302a9af32f8c63dde9e33cdb7d44ea1d9
Pauline GANGLOFF <pganglof@e2r4p2.42.fr>
2020-02-10 14:39:39 +0100

    return status ok

--- srcs/commands/ft_pwd.c
+++ srcs/commands/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: pganglof <pganglof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 12:19:57 by pganglof          #+#    #+#             */
-/*   Updated: 2020/02/10 12:29:31 by pganglof         ###   ########.fr       */
+/*   Updated: 2020/02/10 13:01:25 by pganglof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ int			ft_pwd(t_data *data)
 	char	repertoire[4096];
 
 	if (!(getcwd(repertoire, 4096)))
+	{
+		data->status = 1;
 		return (1);
+	}
 	ft_printf("%s\n", repertoire);
 	dup2(data->savestdout, STDOUT_FILENO);
 	dup2(data->savestdin, STDIN_FILENO);
+	data->status = 0;
 	return (1);
 }
--- srcs/exec_command/right_chevron.c
+++ srcs/exec_command/right_chevron.c
@@ -6,7 +6,7 @@
 /*   By: pganglof <pganglof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 16:22:00 by pganglof          #+#    #+#             */
-/*   Updated: 2020/02/10 10:36:05 by pganglof         ###   ########.fr       */
+/*   Updated: 2020/02/10 13:02:24 by pganglof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void		right_chevron(t_list **tmp, t_data *data)
 		*tmp = (*tmp)->next;
 	while (((t_parsing*)((*tmp)->content))->arg[i + 1] != NULL)
 		i++;
-	if ((data->mypipefd[1] = open(((t_parsing*)((*tmp)->content))->arg[i], O_RDONLY)) == -1)
+	if ((data->mypipefd[1] = open(((t_parsing*)((*tmp)->content))->arg[i],
+	O_RDONLY)) == -1)
 		ft_printf("popo & max: %s: No such file or directory\n",
 		((t_parsing*)((*tmp)->content))->arg[i]);
 	dup2(data->mypipefd[1], STDIN_FILENO);
