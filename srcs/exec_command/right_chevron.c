20733f527350d223d6ad5d38d6f4ab55355e35c2
Pauline GANGLOFF <pganglof@e2r7p17.42.fr>
2020-02-12 12:17:32 +0100

    correction righ_chevron

--- srcs/exec_command/right_chevron.c
+++ srcs/exec_command/right_chevron.c
@@ -6,7 +6,7 @@
 /*   By: pganglof <pganglof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 16:22:00 by pganglof          #+#    #+#             */
-/*   Updated: 2020/02/12 11:50:03 by pganglof         ###   ########.fr       */
+/*   Updated: 2020/02/12 12:11:50 by pganglof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,20 @@ void		right_chevron(t_list **tmp, t_data *data)
 
 	i = 0;
 	while (((t_parsing*)((*tmp)->content))->r_chevron)
-		*tmp = (*tmp)->next;
-	while (((t_parsing*)((*tmp)->content))->arg[i + 1] != NULL)
-		i++;
-	if ((data->mypipefd[1] = open(((t_parsing*)((*tmp)->content))->arg[i],
-	O_RDONLY)) == -1)
 	{
-		ft_printf("popo & max: %s: No such file or directory\n",
-		((t_parsing*)((*tmp)->content))->arg[i]);
-	dup2(data->mypipefd[1], STDIN_FILENO);
+		*tmp = (*tmp)->next;
+		while (((t_parsing*)((*tmp)->content))->arg[i + 1] != NULL)
+			i++;
+		if ((data->mypipefd[1] = open(((t_parsing*)((*tmp)->content))->arg[i],
+		O_RDONLY)) == -1)
+		{
+			ft_putstr_fd("popo & max: ", 2);
+			ft_putstr_fd(((t_parsing*)((*tmp)->content))->arg[i], 2);
+			ft_putstr_fd(": No such file or directory\n", 2);
+			while (((t_parsing*)((*tmp)->content))->r_chevron)
+				*tmp = (*tmp)->next;
+		}
+		else
+			dup2(data->mypipefd[1], STDIN_FILENO);
+	}
 }
