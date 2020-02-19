351165b13859f98bce5c5734c698be2f97652214
Maxime CRAIPEAU <mcraipea@e2r2p20.42.fr>
2020-02-19 15:56:09 +0100

    correctif pour executable et exit unsigned char

--- srcs/commands/ft_exit.c
+++ srcs/commands/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: mcraipea <mcraipea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 16:19:14 by mcraipea          #+#    #+#             */
-/*   Updated: 2020/02/18 16:40:21 by mcraipea         ###   ########.fr       */
+/*   Updated: 2020/02/19 15:15:53 by mcraipea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int			ft_exit(t_parsing *tmp, t_data *data)
 		exit_failure2(0, data);
 	else if (tmp->arg[1] && tmp->arg[2] == NULL)
 	{
-		exit_failure2(ft_atoi(tmp->arg[1]), data);
+		exit_failure2((unsigned char)ft_atoi(tmp->arg[1]), data);
 	}
 	else if (tmp->arg[2])
 	{
